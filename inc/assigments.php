75dede2964fffdebd2eaf4f9a99012c8c250741f
larfoley <lar_x87@yahoo.com>
2017-01-30 23:20:27 +0000

    assigmnets now return a html list of all assigments

--- inc/assigments.php
+++ inc/assigments.php
@@ -1,20 +1,23 @@
 <?php
 
+// Return a the HTML for all the assigments
+
 $current_time = time();
+$assigments = json_decode(file_get_contents('http://duedate.gear.host/api/assigments.php'), true);
 
 function parseAssigments($assigments) {
-
+  // Sort assigments based on due date
   usort($assigments, function($a, $b) {
 
-       if (strtotime($a->due) == strtotime($b->due)) {
+       if (strtotime($a['due']) == strtotime($b['due'])) {
           return 0;
       }
-      return (strtotime($a->due) < strtotime($b->due)) ? -1 : 1;
+      return (strtotime($a['due']) < strtotime($b['due'])) ? -1 : 1;
   });
 
   foreach ($assigments as $assigment) {
     if ($assigment["due"] <= $current_time) {
-      $assigment->overdue = 1;
+      $assigment["overude"] = 1;
     }
   }
 
@@ -34,12 +37,30 @@ function renderAssigments($assigments) {
   foreach ($assigments as $assigment) {
     ?>
     <li class="">
-      <p><?php echo $assigments->name; ?></p>
+      <table>
+        <tr>
+          <th>Subject</th>
+          <td><?php echo $assigment['name']; ?></td>
+        </tr>
+        <tr>
+          <th>Due Date</th>
+          <td><?php echo $assigment['due']; ?></td>
+        </tr>
+        <tr>
+          <th>Details</th>
+          <td><?php echo $assigment['info']; ?></td>
+        </tr>
+        <tr>
+          <th>Overude</th>
+          <td><?php echo $assigment['overdue'] == 1 ? "Yes": "No"; ?></td>
+        </tr>
+      </table>
     </li>
-  }
+    <?php
+  } ?>
 
   </ul>
 
 <?php  }
 
-renderAssigments(parseAssigments(file_get_contents('../api/assigments.php')));
+renderAssigments(parseAssigments($assigments));
