1c1762e2e34731c564e3cbd8e14d455c0a0af5a6
larfoley <lar_x87@yahoo.com>
2017-02-02 10:47:07 +0000

    added footer content for the footer include

---
+++ inc/footer.php
@@ -0,0 +1,12 @@
+  <!-- jQuery ver 3.1.1 -->
+  <script
+    src="https://code.jquery.com/jquery-3.1.1.min.js"
+    integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8=" crossorigin="anonymous">
+  </script>
+  <!-- Bootstrap.js ver 3.3.7 -->
+  <script
+    src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
+    charset="utf-8">
+  </script>
+  </body>
+</html>
