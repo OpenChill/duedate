f6e039dc802da80d75420c80e7336430e218e69b
larfoley <lar_x87@yahoo.com>
2017-02-02 10:46:42 +0000

    added header content for the header include

---
+++ inc/header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+
+    <title>Home</title>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="author" content="Open Chill">
+    <meta name="description" content="Duedate - important dates for Projects & Assignment 's deadline">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet"href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <!--  Master CSS -->
+    <link rel="stylesheet" href="/css/styles.css">
+
+  </head>
+  <body>
